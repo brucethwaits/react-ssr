24f23f9fecaa7fbf0546d054086de7ee8b9e2a97
Bruce Thwaits <brucethwaits@gmail.com>
2019-05-31 10:53:20 +0200

    Cleanup

--- src/server.js
+++ src/server.js
@@ -83,48 +83,5 @@ function htmlTemplate( reactDom, reduxState, helmetData ) {
 
     var result = template(data);
 
-    // var result2 = `
-    //     <!DOCTYPE html>
-    //     <html>
-    //     <head>
-    //         <meta charset="utf-8">
-    //         ${ helmetData.title.toString( ) }
-    //         ${ helmetData.meta.toString( ) }
-    //     </head>
-        
-    //     <body>
-    //         <div id="app">${ reactDom }</div>
-    //         <script>
-    //             window.REDUX_DATA = ${ JSON.stringify( reduxState ) }
-    //         </script>
-    //         <script src="./app.bundle.js"></script>
-    //     </body>
-    //     </html>
-    // `;
-
-    //console.log('result', result);
-    //console.log('result2', result2);
-
     return result;
-    //return result2;
-
-    // return `
-    //     <!DOCTYPE html>
-    //     <html>
-    //     <head>
-    //         <meta charset="utf-8">
-    //         ${ helmetData.title.toString( ) }
-    //         ${ helmetData.meta.toString( ) }
-    //         <title>React SSR</title>
-    //     </head>
-        
-    //     <body>
-    //         <div id="app">${ reactDom }</div>
-    //         <script>
-    //             window.REDUX_DATA = ${ JSON.stringify( reduxState ) }
-    //         </script>
-    //         <script src="./app.bundle.js"></script>
-    //     </body>
-    //     </html>
-    // `;
 }
