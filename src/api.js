d9322120c46bdf01cf67759f42271cd9dc5f6f49
Bruce Thwaits <brucethwaits@gmail.com>
2019-06-02 16:50:53 +0200

    Removed a console.log

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import fetch from "isomorphic-fetch";
 
 export function fetchCircuits(params) {
-    console.log('fetchCircuits', params);
+    //console.log('fetchCircuits', params);
     return fetch( "http://ergast.com/api/f1/2018/circuits.json" )
         .then( res => res.json( ) )
         .then( res => res.MRData.CircuitTable.Circuits );
